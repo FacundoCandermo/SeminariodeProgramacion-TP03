475377ad53d7d80d5069f0e575effd25de60fb0e
FacundoCandermo <kqcandermo0@gmail.com>
2025-05-24 20:45:31 -0300

    Agregar archivos de proyecto.

---
+++ TP03.Entidades/TipoDePago.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP03.Entidades
+{
+    internal class TipoDePago
+    {
+        public int TipoDePagoId { get; set; }
+        public string Descripcion { get; set; } = null!;
+
+        public override string ToString()
+        {
+            return $"{Descripcion}";
+        }
+
+        public TipoDePago Clonar()
+        {
+            return new TipoDePago
+            {
+                TipoDePagoId = TipoDePagoId,
+                Descripcion = Descripcion
+            };
+        }
+    }
+}
